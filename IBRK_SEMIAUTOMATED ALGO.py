22bf66862b516ddc01cf400aac7e6e7dfbdb24aa
tabio1985 <tabio1985@hotmail.com>
2024-06-19 11:54:27 -0400

    Updated function from Yahoo

---
+++ IBRK_SEMIAUTOMATED ALGO.py
@@ -0,0 +1,16 @@
+# Download historical data of Amazon stock from yahoo finance
+import yfinance as yf
+
+def test_yfinance():
+    for symbol in ['AAPL', 'MSFT', 'VFINX', 'BTC-USD']:
+        print(">>", symbol, end=' ... ')
+        try:
+            data = yf.download(symbol, start='2020-09-25', end='2020-10-02')
+            print(data)
+        except Exception as e:
+            print(f"Failed to retrieve data for {symbol}: {e}")
+
+if __name__ == "__main__":
+    test_yfinance()
+
+
